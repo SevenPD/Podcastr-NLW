60924ef8c6a844911179e0f08dd4c946cc81b254
SevenPD <paulodomingues777@gmail.com>
2021-04-26 20:23:09 -0300

    Atualizacao para deploy7

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: process.env.NODE_ENV === "development"
       ? "http://localhost:3333/"
-      : "https://my-json-server.typicode.com/SevenPD/Server/", // seu link aqui dentro das aspas
+      : "https://my-json-server.typicode.com/SevenPD/Server", // seu link aqui dentro das aspas
   });
